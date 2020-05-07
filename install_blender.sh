098700e859189cc7024b5499baabf2703ac08969
Julian Benitez <julian.benitez@mail.escuelaing.edu.co>
2020-05-06 20:46:54 -0500

    Install script

---
+++ install_blender.sh
@@ -0,0 +1,11 @@
+# update
+sudo apt update
+
+# dependencies
+sudo apt-get install libxi6 libgconf-2-4 -y
+sudo apt install libxxf86vm1 -y
+sudo apt-get install libglu1 -y
+sudo apt install libfontconfig1 libxrender1 -y
+
+#blender
+sudo snap install blender --classic
