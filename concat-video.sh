e00e1a636c72c50b5c07c2e1fcb20ff77f74810e
Julian Benitez <julian.benitez@mail.escuelaing.edu.co>
2020-05-20 21:48:41 -0500

    Path fix

--- concat-video.sh
+++ concat-video.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-PATH=/home/ubuntu/videos
+VPATH=/home/ubuntu/videos
 
-if [ -f "$PATH/part1.mp4" ] && [ -f "$PATH/part2.mp4" ] && [ -f "$PATH/part3.mp4" ] && [ -f "$PATH/part4.mp4" ]
+if [ -f "$VPATH/part1.mp4" ] && [ -f "$VPATH/part2.mp4" ] && [ -f "$VPATH/part3.mp4" ] && [ -f "$VPATH/part4.mp4" ]
 then
-    echo file part1.mp4 > mylist.txt
-    echo file part2.mp4 >> mylist.txt
-    echo file part3.mp4 >> mylist.txt
-    echo file part4.mp4 >> mylist.txt
-    ffmpeg -f concat -i mylist.txt -c copy output.mp4
-    mv output.mp4 /home/ubuntu/rendered-projects
+    echo file "$VPATH/part1.mp4" >  "$VPATH/mylist.txt"
+    echo file "$VPATH/part2.mp4" >> "$VPATH/mylist.txt"
+    echo file "$VPATH/part3.mp4" >> "$VPATH/mylist.txt"
+    echo file "$VPATH/part4.mp4" >> "$VPATH/mylist.txt"
+    ffmpeg -f concat -i "$VPATH/mylist.txt" -c copy "$VPATH/output.mp4"
+    mv "$VPATH/output.mp4" /home/ubuntu/rendered-projects
     rm -f /home/ubuntu/videos/part*.mp4
 fi
 
